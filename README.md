bad7d6c16f94925b7e07c679276ca5aab790f982
Luca Sguanci <luca.sguanci@gmail.com>
2014-06-11 11:44:23 +0200

    initial commit - app working fine

---
+++ README.md
@@ -0,0 +1,5 @@
+A simple demo using Router.js
+=========
+
+A simple demo page to instantiate a basic router, built upon [Router.js](https://travis-ci.org/ramiel/router.js) by [Ramiel's creations](http://ramielcreations.com/projects/router-js/ "Ramiel's creations page")
+
---
+++ app/scripts/main.js
@@ -0,0 +1,45 @@
+console.log('\'Allo \'Allo!');
+
+var app = app || {};
+app.Post = [
+  {
+    title: "Today",
+    body: "a really hot day"
+  }, {
+    title: "Tomorrow",
+    body: "another heat-wave hot day"
+  }
+];
+
+$(document).ready(function() {
+  var router = new Router();
+  router.addRoute('#!/posts/:id', function(req,next) {
+    var id = req.params.id;
+    post = app.Post[id];
+    var template = _.template( $("#tpl-post").html() );
+    var cnt = template({post: post});
+    $("div.cnt").html(cnt);
+    document.location.href = req.href;
+  });
+  // router.map(function(match) {
+  //   match("/posts/:id").to("showPost");
+  // });
+
+  // var myHandlers= {};
+  // myHandlers.showPost = {
+  //   model: function(params) {
+  //     return app.Post[id];
+  //   },
+  //   setup: function(post) {
+  //     var template = _.template( $("#tpl-post").html() );
+  //     var cnt = template({post: post});
+  //     $("div.cnt").html(cnt);
+  //   }
+  // }
+  // router.getHandler = function(name) {
+  //   return myHandlers[name];
+  // }
+  // router.updateURL = function(url) {
+  //   window.location.hash = url;
+  // }
+});
